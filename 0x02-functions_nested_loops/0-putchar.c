1df6bb7387eaf3fdd52f952cd1de193cfb4bf08f
Josyi-hub <yipenenye1@gmail.com>
2022-04-12 06:42:03 -0700

    Add 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print _putchar
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Working on functions_nested_loops
